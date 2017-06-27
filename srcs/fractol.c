6b40824a01192d89b4203fc34a2be3d8bdcce02d
sebpalluel <sebpalluel@free.fr>
2017-06-27 15:18:34 +0200

    work - starting to explore fractal calc

--- srcs/fractol.c
+++ srcs/fractol.c
@@ -6,25 +6,24 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/12 20:09:10 by psebasti          #+#    #+#             */
-/*   Updated: 2017/06/27 15:01:09 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/06/27 15:15:39 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
-size_t		ft_fract_calc(t_setup *setup)
+size_t			ft_fract_calc(t_setup *setup)
 {
-	printf("fract_calc f_mode %lu\n", setup->f_mode);
-	//if (SETUP.f_mode == 0 || SETUP.f_mode == 3)
-	//	if (ft_fractol_pthread(setup, 0, ft_mandelbrot) != 0)
-	//		return (0);
-	//if (SETUP.f_mode == 1 || SETUP.f_mode == 3)
-	//	if (ft_fractol_pthread(setup, 1, ft_julia) != 0)
-	//		return (0);
+	if (SETUP.f_mode == 0 || SETUP.f_mode == 3)
+		if (ft_fractol_pthread(setup, 0, ft_mandelbrot) != 0)
+			return (0);
+	if (SETUP.f_mode == 1 || SETUP.f_mode == 3)
+		if (ft_fractol_pthread(setup, 1, ft_julia) != 0)
+			return (0);
 	return (1);
 }
 
-static void		ft_setup_fract_select(char **av, t_setup *setup)
+static size_t	ft_setup_fract_select(char **av, t_setup *setup)
 {
 	if (ft_strcmp(av[1], "mandelbrot") == 0)
 		setup->f_mode = 0;
@@ -34,9 +33,10 @@ static void		ft_setup_fract_select(char **av, t_setup *setup)
 		setup->f_mode = 2;
 	else if (ft_strcmp(av[1], "all") == 0)
 		setup->f_mode = 3;
+	return (ft_fract_calc(setup));
 }
 
-int			main(int ac, char **av)
+int				main(int ac, char **av)
 {
 	t_setup	*setup = NULL;
 
@@ -47,8 +47,8 @@ int			main(int ac, char **av)
 			return (-1);
 		ft_bzero(setup, sizeof(t_setup)); // 2
 		ft_setup_mode(setup, 1);
-		ft_setup_fract_select(av, setup);
-		if (MLX && IMG && SETUP.f_mode != 666)
+		if (MLX && IMG && ft_setup_fract_select(av, setup) \
+				&& SETUP.f_mode != 666)
 			ft_mlx_process(setup);
 		return (ft_setup_mode(setup, 0));
 	}
--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/03 17:50:09 by psebasti          #+#    #+#             */
-/*   Updated: 2017/06/23 14:04:59 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/06/27 15:17:11 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,25 @@
 static int		ft_pthread_process(t_setup *setup, void *(*f)(void *))
 {
 	//pthread_t	thread[NUM_THREAD];
-	int			i;
+	//int			i;
 	int			err;
 
-	i = 0;
+	//i = 0;
 	err = 0;
-	while (i < NUM_THREAD)
-	{
-	//	if ((err = pthread_create(&thread[i], NULL, f, &setup[i + 1])) != 0)
-	//		return (err);
-	f(&setup[i + 1]);
-		i++;
-	}
+	//while (i < NUM_THREAD)
+	//{
+		//	if ((err = pthread_create(&thread[i], NULL, f, &setup[i + 1])) != 0)
+		//		return (err);
+	//	f(&setup[i + 1]);
+	//	i++;
+	//}
 	//while (i > 0)
 	//{
 	//	i--;
 	//	if ((err = pthread_join(thread[i], NULL)) != 0)
 	//		return (err);
 	//}
+	f(&SETUP);
 	return (err);
 }
 
@@ -40,26 +41,28 @@ int				ft_fractol_pthread(t_setup *setup, size_t frac, \
 		void *(*f)(void *))
 {
 	int			err;
-	size_t		i;
-	size_t		j;
-
-	i = 1;
-	j = 0;
-
-	while (i < NUM_THREAD + 1)
-	{
-		ft_memcpy((void *)&(setup[i]).fract[frac], (void *)&(SETUP).fract[frac],\
-				sizeof(t_fract));
-		while (j < 3)
-		{	
-			ft_memcpy((void *)&(setup[i]).fract[frac]->clr[j], \
-					(void *)&(SETUP).fract[frac]->clr[j], sizeof(t_color));
-			j++;
-		}
-		setup[i].fract[frac]->y = 100 * i;
-		setup[i].fract[frac]->y_max = 100 * (i + 1);
-		i++;
-	}
+	//	size_t		i;
+	//	size_t		j;
+	//
+	//	i = 1;
+	//	j = 0;
+	//
+	//while (i < NUM_THREAD + 1)
+	//{
+	//	ft_memcpy((void *)&(setup[i]).fract[frac], (void *)&(SETUP).fract[frac],\
+	//			sizeof(t_fract));
+	//	while (j < 3)
+	//	{	
+	//		ft_memcpy((void *)&(setup[i]).fract[frac]->clr[j], \
+	//				(void *)&(SETUP).fract[frac]->clr[j], sizeof(t_color));
+	//		j++;
+	//	}
+	//	setup[i].fract[frac]->y = 100 * i;
+	//	setup[i].fract[frac]->y_max = 100 * (i + 1);
+	//	i++;
+	//}
+	SETUP.fract[frac]->y = 100;
+	SETUP.fract[frac]->y_max = 200;
 	err = ft_pthread_process(setup, f);
 	return (err);
 }
