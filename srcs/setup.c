21ba7d97b957e8a6312c9541b1539f8670171e48
sebpalluel <sebpalluel@free.fr>
2017-07-11 15:11:20 +0200

    work - error was in color NULL terminated in setup

--- srcs/setup.c
+++ srcs/setup.c
@@ -6,7 +6,7 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/14 17:58:45 by psebasti          #+#    #+#             */
-/*   Updated: 2017/07/11 14:37:57 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/07/11 15:10:51 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,22 @@ static size_t	ft_setup_fract_init(t_setup *setup)
 	size_t j;
 
 	i = 0;
-	if (!(setup->fract = (t_fract **)ft_memalloc(sizeof(t_fract *) * FNUM)))
+	if (!(setup->fract = (t_fract **)ft_memalloc(sizeof(t_fract *) * FNUM + 1)))
 		return (0);
 	while (i < FNUM)
 	{
 		if(!(setup->fract[i] = (t_fract *)ft_memalloc(sizeof(t_fract)))\
 				|| !(setup->fract[i]->clr = (t_color **)\
-					ft_memalloc(sizeof(t_color *) * FNUM)))
+					ft_memalloc(sizeof(t_color *) * 4)))
 			return (0);
 		j = 0;
-		while (j < FNUM)
+		while (j < 3)
 		{
 			if (!(setup->fract[i]->clr[j] = ft_colornew(0, 0, 0)))
 				return (0);
 			j++;
-			setup->fract[i]->clr[j] = NULL;
 		}
+		setup->fract[i]->clr[j] = NULL;
 		i++;
 	}
 	setup->fract[i] = NULL;
@@ -55,7 +55,7 @@ static void		ft_setup_delete(size_t i, t_setup *setup)
 		while (++frac_n < FNUM)
 		{
 			col_n = -1;
-			while (++col_n < FNUM)
+			while (++col_n < 4)
 				free (setup[i].fract[frac_n]->clr[col_n]);
 			free (setup[i].fract[frac_n]);
 		}
